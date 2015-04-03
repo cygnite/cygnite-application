41f0c2b9a68739084f88653b176a8524b0aaf0f4
Sanjoy Dey <sanjoy09@hotmail.com>
2015-04-03 19:57:34 +0530

    Session config updated

--- apps/configs/session.php
+++ apps/configs/session.php
@@ -3,77 +3,94 @@ if (!defined('CF_SYSTEM')) {
     exit('External script access not allowed');
 }
 /**
- *  Cygnite Framework
- *  Database Configuration Settings
+ * Cygnite PHP Framework
  *
- *  An open source application development framework for PHP 5.3x or newer
+ * Session Configuration
  *
- *   License
- *
- *   This source file is subject to the MIT license that is bundled
- *   with this package in the file LICENSE.txt.
- *   http://www.appsntech.com/license.txt
- *   If you did not receive a copy of the license and are unable to
- *   obtain it through the world-wide-web, please send an email
- *   to sanjoy@hotmail.com so I can send you a copy immediately.
- *
- *@package               : Apps
- *@subpackages           : Configurations
- *@filename              : session.php
- *@description           : You can set your session configurations here.
- *@author                : Sanjoy Dey
- *@copyright             : Copyright (c) 2013 - 2014,
- *@link	                 : http://www.cygniteframework.com
- *@since	             : Version 1.2
- *@filesource
- *@warning               :  If you don't protect this directory from direct web access,
- * anybody will be able to see your database configuration and settings.
- *
- *
+ * @author Sanjoy Dey <dey.sanjoy0@gmail.com>
  */
 
-
 return array(
-   /**
-    *--------------------------------------------------------------------------
-    * Session Config
-    *--------------------------------------------------------------------------
-    * You can auto enable/disable cygnite session mechanism by
-    * setting cf_session as true/false.
-    * Cygnite will take care of rest.
-    */
-    'cf_session'       => false , //Set true or false to start session default is false
 
-   /**
-    *--------------------------------------------------------------------------
-    * Session Name
-    *--------------------------------------------------------------------------
-    * You can set your application session name here.
-    * Cygnite will take care of rest.
-    */
-    'cf_session_name'  => 'cf_secure_session',
+    /**
+     *----------------------------------------------------------
+     * Session Driver
+     *----------------------------------------------------------
+     * Set session driver to use native session or database
+     * based session. Just provide the driver as "native" or
+     * "database". Cygnite will take care of next.
+     */
+    'driver' => 'native', // native, database
 
-    /*
-    *--------------------------------------------------------------------------
-    * Session Name
-    *--------------------------------------------------------------------------
-    * You can set your application default session storage path here. By default
-    * All your session details will be store on apps/temp/sessions/ directory.
-    * Keep save path as default.
-    */
-    'cf_session_save_path' => 'default', // Framework default Session path is apps/temp/sessions/
+    /**
+     *----------------------------------------------------------
+     * Session Name
+     *----------------------------------------------------------
+     * Set your session name, else cygnite will use default name
+     */
+    'session_name'  => 'cf_secure_session',
 
+    /**
+     *----------------------------------------------------------
+     * Session Storage Path
+     *----------------------------------------------------------
+     * Leave it as default as it will store into the default session
+     * directory.
+     */
+    'path' => 'default',
 
-    /*
-    *--------------------------------------------------------------------------
-    * All these below configurations and features will be available on next versions
-    *--------------------------------------------------------------------------
-    *
-    */
-    'cf_session_cookie_name'   => '',    //Need to update code for next version
-    'cf_session_timeout'       => 1440,  //Need to update code for next version
-    'cf_session_auto_start'    => '',    //Need to update code for next version
-    'cf_session_use_db'        => false, // Need to update code for next version
-    'cf_session_db_name'       => '',    // Need to update code for next version
-    'cf_session_table_name'    => 'cf_session', // Need to update code for next version
+    /**
+     *----------------------------------------------------------
+     * Database For Session Storage
+     *----------------------------------------------------------
+     * Set the database name if you are using database based session
+     * in order to store session.
+     */
+    'database_name' => 'cygnite',
+
+    /**
+     *----------------------------------------------------------
+     * Setting Table Name For Session Storage
+     *----------------------------------------------------------
+     * Provide table name if you are using database based session.
+     * By default "cf_sessions" is the table to store all session
+     * into table.
+     */
+    'table' => 'cf_sessions',
+
+    /**
+     *----------------------------------------------------------
+     * Use Session Cookie
+     *----------------------------------------------------------
+     * Configure to use session cookie or not
+     */
+    'use_session_cookie' => false,
+
+    /**
+     *----------------------------------------------------------
+     * Session Cookie Name
+     *----------------------------------------------------------
+     * If you are using session cookie then provide the name here else
+     * leave it blank.
+     */
+    'session_cookie_name' => '',
+
+    /**
+     *----------------------------------------------------------
+     * HTTP Only
+     *----------------------------------------------------------
+     * Configure to set httponly for cookie params
+     */
+    'httponly' => true,
+
+    'timeout' => 1440,
+
+    /**
+     *----------------------------------------------------------
+     * Secure Session
+     *----------------------------------------------------------
+     * You may leave it as false or true to use secure session
+     * By default cygnite provides most security to the session.
+     */
+    'secure' => false,
 );
