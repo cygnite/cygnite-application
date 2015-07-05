626061588394f2d0b00893f09515c7024f9780e8
Sanjoy Dey <sanjoy09@hotmail.com>
2015-07-05 13:28:34 +0530

    Seeders modified as per the seeder command update. Seeders specified as name instead of namespace in DatabaseTable

--- src/Apps/Resources/Database/Seeding/DatabaseTable.php
+++ src/Apps/Resources/Database/Seeding/DatabaseTable.php
@@ -13,13 +13,33 @@ class DatabaseTable extends Seeder
     /**
      * Specify all seeder class namespace here
      * Seeder will seed data into corresponding database table.
+     *
      * @var array
      */
     protected $seeders = [
-        'Apps\Resources\Database\Seeding\UserTable',
+        'UserTable',
+        'RoleTable',
     ];
 
     /**
+     * Run all seeders to seed database
+     *
+     * Example Command:
+     * <code>
+     *
+     * php cygnite database:seed
+     *
+     * </code>
+     *
+     * Run only particular seeder
+     *
+     * Example Command:
+     * <code>
+     *
+     * php cygnite database:seed UserTable,RoleTable
+     *
+     * </code>
+     *
      * We will run all seeder class to seed database table
      */
     public function run()
--- src/Apps/Resources/Database/Seeding/RoleTable.php
+++ src/Apps/Resources/Database/Seeding/RoleTable.php
@@ -2,22 +2,19 @@
 namespace Apps\Resources\Database\Seeding;
 
 use Cygnite\Database\Migration;
-use Cygnite\Database\Table\Schema;
-use Cygnite\Database\Table\Seeder;
-
 /**
  * Class UserTable
  *
  * @package Apps\Resources\Database\Seeding
  */
-class UserTable extends Migration
+class RoleTable extends Migration
 {
     public function execute()
     {
         $data = [
-            'name' => '',
-            'description' => '',
-            'meta' => '',
+            'role_name' => '',
+            'role_description' => '',
+            'role_meta' => '',
             'created_by' => '',
         ];
 
