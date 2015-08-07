72788a11e316880b7f9fe997015778814588825f
Sanjoy Dey <sanjoy09@hotmail.com>
2015-08-08 02:16:03 +0530

    Removed DefinitionManagerTrait and replaced with array of configuration

---
+++ src/Apps/Configs/definitions/configuration.php
@@ -0,0 +1,44 @@
+<?php
+if (!defined('CF_SYSTEM')) {
+    exit('External script access not allowed');
+}
+
+return [
+    /**
+     * Set controller property dependencies here.
+     * Cygnite will inject all your dependencies at runtime
+     *
+     * @return array
+     *  <code>
+     *  'property.definition' [
+     *          'HomeController' => [
+     *              'api' => 'apps.resources.extensions.api'
+     *           ),
+     *          'ProductController' => [
+     *             'social' => 'apps.resources.extensions.social-share',
+     *          ],
+     *  ];
+     *  </code>
+     *
+     */
+    'property.definition' =>  [
+        'HomeController' => [
+            'social' => 'apps.resources.extensions.social-share',
+        ],
+    ],
+
+    /**
+     *
+     * @return type
+     *  <code>
+     *  'register.alias' =>  [
+     *         'GeneralInterface' => '\\Apps\\Extensions\\General',
+     *         'ORM' => '\\Cygnite\\Database\\ActiveRecord',
+     *  ];
+     *  </code>
+     */
+
+    'register.alias' => [
+        'CustomInterface' => 'Apps\Resources\Extensions\Custom'
+    ]
+];
