16186a150e7310c37781da8355babd424acc42d9
Serghei Iakovlev <sergeyklay@users.noreply.github.com>
2017-01-23 21:19:53 +0200

    Update initialize.php

--- bootstrap/initialize.php
+++ bootstrap/initialize.php
@@ -11,20 +11,21 @@
 require __DIR__ . "/../vendor/autoload.php";
 
 /*
-|--------------------------------------------------------------------------
-| Create The Application
-|--------------------------------------------------------------------------
-|
-| To boot framework first thing we will create a new application instance
-| which serves glue for all the components, and binding components
-| with the IoC container
+ |-------------------------------------------------------------------
+ | Create The Application
+ |-------------------------------------------------------------------
+ |
+ | To boot framework first thing we will create a new application
+ | instance which serves glue for all the components, and binding
+ | components with the IoC container.
 */
 $app = new \Cygnite\Foundation\Application();
-/**
- | ---------------------------------------------------
+
+/*
+ |-------------------------------------------------------------------
  | Application booting process
- | --------------------------------------------------
- *
- * Set configuration and services and boot-up application
+ |-------------------------------------------------------------------
+ |
+ | Set configuration and services and boot-up application.
  */
 return $app->configure();
